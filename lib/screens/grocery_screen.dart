1cf8d4d84f10452150d5ca0bbdd98ecd2af84088
DESKTOP-LCD\lcd26 <dan.luongcong@gameloft.com>
2024-04-19 10:23:11 +0700

    + new item screen

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shopping/screens/new_item_screen.dart';
 
 import 'package:shopping/widgets/grocery_list.dart';
--- lib/screens/new_item_screen.dart
+++ lib/screens/new_item_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:shopping/data/categories.dart';
+import 'package:shopping/models/category.dart';
+
+import 'package:shopping/utils/thousands_formatter.dart';
 
 class NewItemScreen extends StatefulWidget {
   const NewItemScreen({super.key});
@@ -14,10 +20,70 @@ class _NewItemScreenState extends State<NewItemScreen> {
       appBar: AppBar(
         title: const Text('Add New Item'),
       ),
-      body: const Padding(
+      body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Text('form here'),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Name',
+              ),
+              keyboardType: TextInputType.text,
+              maxLength: 50,
+              validator: (value) {
+                if (value == null || value.trim().isEmpty) {
+                  return 'Please enter a name';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              initialValue: '1',
+              decoration: const InputDecoration(
+                labelText: 'Quantity',
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                ThousandsFormatter(),
+              ],
+            ),
+            DropdownButtonFormField(
+              items: _buildCategoryItems(),
+              onChanged: (selected) {
+                debugPrint('Selected: $selected');
+              },
+              decoration: const InputDecoration(
+                labelText: 'Category',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Add Item'),
+            ),
+          ],
+        )),
       ),
     );
+  }
+
+  List<DropdownMenuItem<Category>> _buildCategoryItems() {
+    return categories.entries.map((item) {
+      return DropdownMenuItem<Category>(
+        value: item.value,
+        child: Row(
+          children: [
+            Container(
+              width: 16,
+              height: 16,
+              color: item.value.color,
+            ),
+            const SizedBox(width: 8),
+            Text(item.value.name),
+          ],
+        ),
+      );
+    }).toList();
   }
 }
